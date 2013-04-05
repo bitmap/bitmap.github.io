4f276a6c0ec7d74f03e0e9f463c686ad256e50f2
Cabe <caleb.branson@gmail.com>
2013-04-05 01:22:07 -0700

    updating slideshow script (WIP)

--- js/cabe.js
+++ js/cabe.js
@@ -1,11 +1,48 @@
-$(document).ready(function() {
-	$("#work-link").click(function (e) {
-		$("#portfolio").addClass("in-focus");
-		$("header").addClass("in-background");
-	});
-	$("#back").click(function () {
-		$('#portfolio').removeClass("in-focus");
-		$("header").removeClass("in-background");
-	});
-});
+(function () {
+    'use strict'; //JSHint
 
+    var workLink = document.getElementById('work-link'),
+        portfolio = document.getElementById('portfolio'),
+        header = document.getElementById('splash'),
+        back = document.getElementById('back'),
+        img = document.getElementsByTagName('img'),
+        fig = document.getElementsByTagName('figure'),
+
+        doWork = function () {
+            portfolio.className = 'in-focus';
+            header.className = 'in-background';
+        },
+
+        hideWork = function () {
+            portfolio.className = '';
+            header.className = '';
+        },
+
+        nextImage = function() {
+            return function() {
+                for (var i = 0; i <= fig.length; i++) {
+                    var figLoop = fig[i];
+
+                    if ( figLoop.lastElementChild.className === 'active' ) {
+                       figLoop.firstElementChild.className = 'active';
+                       //figLoop.getElementsByTagName("img").className = '';
+                       console.log("if");
+                    }
+                    else {
+                        fig.getElementsByClassName("active").nextElementSibling.className = 'active';
+                        // this.className = '';
+                        console.log('else');
+                    }
+                }
+            };
+        };
+
+    workLink.addEventListener('click', doWork);
+    back.addEventListener('click', hideWork);
+
+    for (var i = 0; i < img.length; i++) {
+        var item = img[i];
+        item.onclick = nextImage(item);
+    }
+
+}());
