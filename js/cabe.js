7304b5984f47b3ac524512dd621556e4505bedc1
Cabe <caleb.branson@gmail.com>
2013-04-05 20:33:33 -0700

    lazy load ftw

--- js/cabe.js
+++ js/cabe.js
@@ -1,8 +1,14 @@
 $(function () {
     'use strict';
+    $('img').lazyload({
+        threshold: 200
+    });
+
+
     $('#work-link').click(function () {
         $('#portfolio').addClass('in-focus');
         $('header').addClass('in-background');
+        $(window).trigger('scroll');
     });
 
     $('#back').click(function () {
@@ -10,12 +16,23 @@ $(function () {
         $('header').removeClass('in-background');
     });
     $('figure img').click(function () {
+
         if ($(this).is(':last-child')) {
             $(this).parent().find(':first-child').addClass('active');
-            $(this).removeClass('active');
+            $(this).removeClass('active').css('');
+            $(this).trigger('scroll');
+
         } else {
             $(this).next('img').addClass('active');
-            $(this).removeClass('active');
+            $(this).removeClass('active').css('');
+            $(this).trigger('scroll');
         }
     });
+    $("img", "figure").each(function() {
+    $(this).load(function() {
+        var width = $(this).width();
+        var height = $(this).height();
+        $(this).parent().css("height", height);
+    });
+});
 });
