a887fec1a55888123a2927d65e5df57336827960
Cabe <caleb.branson@gmail.com>
2013-04-05 17:10:24 -0700

    tidy up

--- js/cabe.js
+++ js/cabe.js
@@ -1,21 +1,21 @@
-$(function() {
-     $('#work-link').click( function() {
+$(function () {
+    'use strict';
+    $('#work-link').click(function () {
         $('#portfolio').addClass('in-focus');
         $('header').addClass('in-background');
-     })
+    });
 
-    $('#back').click( function() {
+    $('#back').click(function () {
         $('#portfolio').removeClass('in-focus');
         $('header').removeClass('in-background');
-     })
-    $('figure img').click( function() {
+    });
+    $('figure img').click(function () {
         if ($(this).is(':last-child')) {
             $(this).parent().find(':first-child').addClass('active');
             $(this).removeClass('active');
-        }
-        else {
+        } else {
             $(this).next('img').addClass('active');
             $(this).removeClass('active');
         }
-    })
+    });
 });
