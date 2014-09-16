3363afa64c0ec468584e6a0f7fc6100000aa995f
Cabe <caleb.branson@gmail.com>
2014-09-16 11:36:03 -0700

    refactor gallery script

--- js/scripts.js
+++ js/scripts.js
@@ -1,86 +1,76 @@
-(function () {
-  'use strict';
+(function() {
+    "use strict";
+    var makeWerk = function(werk) {
 
-    var makeGallery = function(gallery) {
+        function tabClick(e) {
+            var index = e.target.getAttribute("data-index"), 
+                imageCollection = werk.querySelectorAll("img"), 
+                elem, j;
 
-    function createEl(el) {
-      return document.createElement(el);
-    }
-
-    function tabClick(e) {
-      var index = e.target.getAttribute("data-index"),
-          imageCollection = gallery.querySelectorAll("img"),
-          elem, j;
-
-      if (tabItems[index].classname = "active") {
-
-        for (j = 0; j < imageCollection.length; j++) {
-          elem = imageCollection[j];
-
-          if (j == index) {
-
-            if (elem.hasAttribute("data-src")) {
-              elem.setAttribute('src', elem.getAttribute('data-src'));
-              elem.removeAttribute('data-src');
+            if (tabItems[index].classname = "active") {
+                for (j = 0; j < imageCollection.length; j++) {
+                    elem = imageCollection[j];
+                    if (j == index) {
+                        if (elem.hasAttribute("data-src")) {
+                            elem.setAttribute("src", elem.getAttribute("data-src"));
+                            elem.removeAttribute("data-src");
+                        }
+                        elem.classList.add("active");
+                        tabItems[j].classList.add("active");
+                    } else {
+                        tabItems[j].classList.remove("active");
+                        elem.classList.remove("active");
+                    }
+                }
             }
-
-            elem.classList.add("active");
-            tabItems[j].classList.add("active");
-          }
-
-          else {
-            tabItems[j].classList.remove("active");
-            elem.classList.remove("active");
-          }
         }
-      }
+
+        werk.appendChild(document.createElement("ul")).className = "tabs";
+        werk.appendChild(document.createElement("div")).className = "close";
+
+        var tabHolder = werk.querySelector(".tabs"), 
+            tabItems = tabHolder.children, 
+            tabCount = werk.querySelectorAll("img").length, 
+            elem, i;
+        for (i = 0; i < tabCount; i++) {
+            elem = document.createElement("li");
+            tabHolder.appendChild(elem);
+            elem.setAttribute("data-index", i);
+            elem.innerHTML = i + 1;
+            elem.addEventListener("click", tabClick);
+        }
+
+        var img = werk.querySelector("img");
+        img.setAttribute("src", img.getAttribute("data-src"));
+        img.removeAttribute("data-src");
+        img.classList.add("active");
+        tabItems[0].classList.add("active");
+
+        var closeBtn = werk.querySelector(".close");
+
+        closeBtn.innerHTML = "×";
+        closeBtn.addEventListener("click", function() {
+            werk.classList.remove("is-visible");
+        });
+    };
+
+    function getWerk(el) {
+        el.addEventListener("click", function() {
+            var href = el.getAttribute("href").replace("#", "");
+            document.getElementById(href).classList.add("is-visible");
+        });
     }
 
-    gallery.appendChild(createEl('ul')).className = "tabs";
-    gallery.appendChild(createEl('div')).className = "close";
+    var tabContainer = document.querySelectorAll(".werk"), 
+        link = document.querySelectorAll(".werk-link"),
+        each = Array.prototype.forEach;
 
-    var closeBtn = gallery.querySelector('.close');
-
-    var tabHolder = gallery.querySelector(".tabs"),
-      tabItems = tabHolder.children,
-      tabCount = gallery.querySelectorAll("img").length,
-      elem, i;
-
-    for (i = 0; i < tabCount; i++) {
-      elem = createEl("li");
-      tabHolder.appendChild(elem);
-      elem.setAttribute("data-index", i);
-      elem.innerHTML = i + 1;
-      elem.addEventListener("click", tabClick);
-    }
-
-    var img = gallery.querySelector("img");
-    img.setAttribute('src', img.getAttribute('data-src'));
-    img.removeAttribute('data-src');
-    img.classList.add("active");
-    tabItems[0].classList.add("active");
-
-    closeBtn.addEventListener('click', function() {
-      gallery.classList.remove('is-visible');
-    })
-  };
-
-  function findGallery(el) {
-    el.addEventListener("click", function() {
-      var href = el.getAttribute('href').replace('#','');
-      document.getElementById(href).classList.add('is-visible')
+    each.call(tabContainer, function(el) {
+        makeWerk(el);
     });
-  }
 
-  var tabContainer = document.querySelectorAll('.gallery'),
-      link = document.querySelectorAll('.launch');
-
-  Array.prototype.forEach.call(tabContainer, function(el) {
-    makeGallery(el);
-  });
-
-  Array.prototype.forEach.call(link, function(el) {
-    findGallery(el);
-  });
+    each.call(link, function(el) {
+        getWerk(el);
+    });
 
 })();
