30c431d6bde1ce47844289fdc1dc146a0211e192
Cabe Branson <caleb.branson@gmail.com>
2015-08-19 11:19:22 -0700

    use gulp to concat EVERYTHING

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,101 @@
-!function(){"use strict";var e=document.querySelectorAll(".bokeh"),t=document.querySelectorAll(".bokeh-link"),a=document.querySelector(".bokeh-blur"),i=Array.prototype.forEach,c={init:function(e){var t,a,i=this;t=e.querySelectorAll("img"),i.createTabs(e,t),i.createCloseButton(e),a=e.querySelectorAll(".bokeh__tabs > li"),i.setActive(a[0],t[0])},createTabs:function(e,t){e.appendChild(document.createElement("ul")).className="bokeh__tabs";var a,n=e.querySelector(".bokeh__tabs");i.call(t,function(e,t){a=document.createElement("li"),n.appendChild(a),a.setAttribute("data-index",t),e.setAttribute("data-index",t),a.innerHTML=t+1,c.tabClickHandler(a,e)})},createCloseButton:function(e){e.appendChild(document.createElement("div")).className="bokeh__close";var t=e.querySelector(".bokeh__close");t.innerHTML="CLOSE",t.addEventListener("click",function(){e.classList.remove("is-visible"),a&&a.classList.remove("has-blur")})},tabClickHandler:function(e,t){e.addEventListener("click",function(){e.getAttribute("data-index")===t.getAttribute("data-index")&&c.setActive(e,t)})},setActive:function(e,t){function a(e){return e.parentNode.querySelector(".is-active")}var i="is-active";null!==a(e)&&a(e).classList.remove(i),null!==a(t)&&a(t).classList.remove(i),t.hasAttribute("data-src")&&(t.setAttribute("src",t.getAttribute("data-src")),t.removeAttribute("data-src")),e.classList.add(i),t.classList.add(i)},showModal:function(e){e.addEventListener("click",function(){var t=e.getAttribute("href").replace("#",""),i=document.getElementById(t);i.classList.add("is-visible"),a&&a.classList.add("has-blur")})}};i.call(e,function(e){c.init(e)}),i.call(t,function(e){c.showModal(e)})}();
+(function() {
+  'use strict';
+
+  var containers = document.querySelectorAll('.bokeh')
+  ,   anchors = document.querySelectorAll('.bokeh-link')
+  ,   blurEl = document.querySelector('.bokeh-blur')
+  ,   each = Array.prototype.forEach
+  ;
+
+  var bokeh = {
+    init: function(el) {
+      var bokeh = this
+      , imgs, tabs
+      ;
+
+      // Create DOM elements
+      imgs = el.querySelectorAll('img');
+      bokeh.createTabs(el, imgs);
+      bokeh.createCloseButton(el);
+
+      // Attach events
+      tabs = el.querySelectorAll('.bokeh__tabs > li');
+      bokeh.setActive(tabs[0], imgs[0]);
+    },
+
+    createTabs: function(el, imgs) {
+      // Make a container for the tabs
+      el.appendChild(document.createElement('ul')).className = 'bokeh__tabs';
+      var tabs = el.querySelector('.bokeh__tabs'), li;
+
+      // Make a tab for each image
+      each.call(imgs, function(img, idx) {
+        li = document.createElement('li');
+        tabs.appendChild(li);
+        li.setAttribute('data-index', idx);
+        img.setAttribute('data-index', idx);
+        li.innerHTML = idx + 1;
+        bokeh.tabClickHandler(li, img);
+      });
+    },
+
+    createCloseButton: function(el) {
+      // Make a close button
+      el.appendChild(document.createElement('div')).className = 'bokeh__close';
+      var closeBtn = el.querySelector('.bokeh__close');
+      closeBtn.innerHTML = 'CLOSE';
+
+      // Attach click handler to close modal
+      closeBtn.addEventListener('click', function() {
+       el.classList.remove('is-visible');
+       if (blurEl) blurEl.classList.remove('has-blur');
+      });
+    },
+
+    tabClickHandler: function(tab, img) {
+      tab.addEventListener('click', function() {
+        if (tab.getAttribute('data-index') === img.getAttribute('data-index')) {
+          bokeh.setActive(tab, img);
+        }
+      });
+    },
+
+    setActive: function(tab, img) {
+      var ACTIVE_CLASS = 'is-active';
+
+      if (parent(tab) !== null) parent(tab).classList.remove(ACTIVE_CLASS);
+      if (parent(img) !== null) parent(img).classList.remove(ACTIVE_CLASS);
+
+      if (img.hasAttribute('data-src')) {
+        img.setAttribute('src', img.getAttribute('data-src'));
+        img.removeAttribute('data-src');
+      }
+      tab.classList.add(ACTIVE_CLASS);
+      img.classList.add(ACTIVE_CLASS);
+
+      function parent(e) {
+        return e.parentNode.querySelector('.is-active');
+      }
+    },
+
+    showModal: function(el) {
+      el.addEventListener('click', function() {
+        var href = el.getAttribute('href').replace('#', '')
+        ,   modal = document.getElementById(href)
+        ;
+
+        modal.classList.add('is-visible');
+        if (blurEl) blurEl.classList.add('has-blur');
+      });
+    }
+  };
+
+  each.call(containers, function(el) {
+    bokeh.init(el);
+  });
+
+  each.call(anchors, function(el) {
+    bokeh.showModal(el);
+  });
+
+})();
